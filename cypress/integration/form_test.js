00b1d1f024e658b10a3739d6c2f592682c57cda8
cknoettg <cknoettg@yahoo.com>
2020-04-27 00:19:42 -0400

    Added Form Validation test. MVP Complete

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,6 +3,8 @@ describe("Testing our form submission...", function(){
         cy.visit("http://localhost:3000/");
     });
     it("Testing our input values...", () => {
+        //cy.get('formSchema.isValid:true').should('have.length', 0)
+        cy.get("button").should('be.disabled');
         cy.get('input[name="name"]')
             .type("Corey")
             //note: should is our assert
@@ -17,5 +19,7 @@ describe("Testing our form submission...", function(){
             .check()
             .should("be.checked");
         cy.get("button").click();
+        cy.get("button").should('not.be.disabled');
+        //cy.get('input:invalid').should('have.length', 1)
     }); //end of it statement
 }); //end of describe
