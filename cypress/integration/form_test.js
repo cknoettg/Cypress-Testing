520b60ed2916832fd1bc56a0005cea473cc36cf0
cknoettg <cknoettg@yahoo.com>
2020-04-29 23:52:36 -0400

    Added tests for empty inputs. MVP is really complete now.

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,23 +3,30 @@ describe("Testing our form submission...", function(){
         cy.visit("http://localhost:3000/");
     });
     it("Testing our input values...", () => {
-        //cy.get('formSchema.isValid:true').should('have.length', 0)
         cy.get("button").should('be.disabled');
+        //test for when name is empty
+        cy.get('input[name="name"]').should('have.value', '');
         cy.get('input[name="name"]')
             .type("Corey")
             //note: should is our assert
             .should("have.value", "Corey");
+        //test for when email is empty
+        cy.get('input[name="email"]').should('have.value', '');
         cy.get('input[name="email"]')
             .type("email@email.com")
             .should("have.value", "email@email.com");
+        //test for when password is empty
+        cy.get('input[name="password"]').should('have.value', '');
         cy.get('input[name="password"]')
             .type("123456")
             .should("have.value", "123456");
+        //test for when checkbox is not checked
+        cy.get('[type="checkbox"]')
+            .should("not.be.checked");
         cy.get('[type="checkbox"]')
             .check()
             .should("be.checked");
         cy.get("button").click();
         cy.get("button").should('not.be.disabled');
-        //cy.get('input:invalid').should('have.length', 1)
     }); //end of it statement
 }); //end of describe
